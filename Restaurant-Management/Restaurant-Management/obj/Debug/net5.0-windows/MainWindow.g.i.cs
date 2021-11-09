933ada17dfb292e4b3a8af87c04f105b3d2f7932
teodoramz <amzteodor@gmail.com>
2021-11-09 23:46:20 +0200

    some deleted stuff

--- Restaurant-Management/Restaurant-Management/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ Restaurant-Management/Restaurant-Management/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -77,7 +77,7 @@ namespace Restaurant_Management {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Restaurant-Management;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Restaurant-Management;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
