99080266e0ec1649c6e51d9e34304c53ecc8096d
Stefan  Joita <joitastefan8@gmail.com>
2021-11-16 19:39:56 +0200

    register+dashboard
    register+dashboard

---
+++ Restaurant-Management/Restaurant-Management/DashboardWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Restaurant_Management
+{
+    /// <summary>
+    /// Interaction logic for DashboardWindow.xaml
+    /// </summary>
+    public partial class DashboardWindow : Window
+    {
+        public DashboardWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void categoriesTextButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void productsTextButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void registration_codesTextButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void historyTextButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void employessTextButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Restaurant-Management/Restaurant-Management/RegisterWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Restaurant_Management
+{
+    /// <summary>
+    /// Interaction logic for RegisterWindow.xaml
+    /// </summary>
+    public partial class RegisterWindow : Window
+    {   //database connection variables
+        static String connectionString = "Server=.;Database=Restaurant-Management;Trusted_Connection=true";
+        SqlConnection connection = new SqlConnection(connectionString);
+        DataSet DS = new DataSet();
+        SqlDataAdapter DA = new SqlDataAdapter();
+        public RegisterWindow()
+        {
+            InitializeComponent();
+            clearTextBoxes();
+        }
+        //clear all text boxes
+        public void clearTextBoxes()
+        {
+            usernameTextBox.Clear();
+            passTextBox.Clear();
+            pass_againTextBox.Clear();
+            phoneTextBox.Clear();
+            emailTextBox.Clear();
+            verification_codeTextBox.Clear();
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            var username = usernameTextBox.Text;
+            var password = passTextBox.Text;
+            var password_again = pass_againTextBox.Text;
+            var phone = phoneTextBox.Text;
+            var email = emailTextBox.Text;
+            var verification_code = verification_codeTextBox.Text;
+            //checking if confirmation password is ok
+            if (password_again!=password)
+            {   
+                MessageBox.Show("Password not matching");
+                return;
+            }
+
+
+        }
+
+        private void ExitButtonClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
