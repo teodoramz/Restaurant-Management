9039156b333ab8c15c474fc7a459109c5354880e
liviupopa16 <liviu.popa@mta.ro>
2021-11-17 20:21:06 +0200

    Admin page connections

--- Restaurant-Management/Restaurant-Management/AdminCategoriesWindow.xaml.cs
+++ Restaurant-Management/Restaurant-Management/AdminCategoriesWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,9 +20,45 @@ namespace Restaurant_Management
     /// </summary>
     public partial class AdminCategoriesWindow : Window
     {
+
+        int loggedUserID;
         public AdminCategoriesWindow()
         {
             InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void logoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow lw = new LoginWindow();
+            lw.Show();
+            this.Hide();
+        }
+
+        private void dashboardButton_Click(object sender, RoutedEventArgs e)
+        {
+            DashboardWindow dw = new DashboardWindow(loggedUserID);
+            dw.Show();
+            this.Hide();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void idTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^1-9][0-9]*");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
--- Restaurant-Management/Restaurant-Management/AdminHistoryWindow.xaml.cs
+++ Restaurant-Management/Restaurant-Management/AdminHistoryWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,9 +20,60 @@ namespace Restaurant_Management
     /// </summary>
     public partial class AdminHistoryWindow : Window
     {
+        int loggedUserID;
+
         public AdminHistoryWindow()
         {
             InitializeComponent();
+        }
+
+        private void dashboardButton_Click(object sender, RoutedEventArgs e)
+        {
+            DashboardWindow dw = new DashboardWindow(loggedUserID);
+            dw.Show();
+            this.Hide();
+        }
+
+        private void logoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow lw = new LoginWindow();
+            lw.Show();
+            this.Hide();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void idUserTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^1-9][0-9]*");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void totalPriceTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9][.0-9]*");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void editButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
